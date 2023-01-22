c51aa39b332efa1ebba34c7388530721e3f88f5a
UniversoCaotico1997 <loria.roberto97@gmail.com>
2023-01-22 16:28:26 +0100

    Import axios + call Ajax

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
     components: {
     },
     data() {
         return {
             projects: null,
-            baseUrlApi: 'http://localhost:8000'
+            base_url_api: 'http://localhost:8000',
+            loading: true,
+            error: null
 
         }
     },
-    method: {
-        getProjetcs() {
-
+    methods: {
+        getProjects(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data.results);
+                    this.projects = response.data.results;
+                    this.loading = false;
+                })
+                .catch(error => {
+                    console.error(error);
+                    this.error = error.message;
+                    this.loading = false;
+                })
         }
     },
     mounted() {
-        this.getProjetcs();
+        this.getProjects(this.base_url_api + '/api/projects');
     }
 }
 </script>
