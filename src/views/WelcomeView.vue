d73b35f53eb896e22b0d75200b9e069441f6a932
UniversoCaotico1997 <loria.roberto97@gmail.com>
2023-01-26 18:07:35 +0100

    refactoring WelcomeView

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,32 +1,45 @@
 <script>
-import Jumbotron from "../components/jumbotron.vue";
 export default {
-    name: 'WelcomeView',
-    components: {
-        Jumbotron
-    }
+    name: 'WelcomeView'
 }
 
 </script>
 
 <template>
-    <div class="container">
-        <router-link :to="{ name: 'home' }">
-            <div
-                class="jumbotron jumbotron-fluid position-absolute top-50 start-50 translate-middle border rounded shadow p-3 mb-5 text-white text-center bg-dark">
-                <h1 class="display-4">
-                    Welcome In My Space
+    <router-link :to="{ name: 'home' }" class="text-decoration-none">
+        <div class="container d-flex justify-content-center align-items-center">
+            <div class="square border rounded shadow text-center text-white position-relative">
+                <h1 class="display-4 mt-3">
+                    Welcome
+                    <br> In
+                    <br> My Space
                 </h1>
-                <p class="lead">
+                <h5 class="mt-5">
                     Click For See More
-                </p>
+                </h5>
+                <img src="../assets/img/logo.png" class="w-100 position-absolute top-50 start-50 translate-middle"
+                    alt="logo">
             </div>
-        </router-link>
-    </div>
+        </div>
+    </router-link>
 </template>
 
 <style lang="scss" scoped>
 .container {
-    background-image: url(../assets/img/Splash.png);
+    height: 100vh;
+}
+
+.square {
+    width: 600px;
+    height: 200px;
+    background-color: black;
+    transition: width 1s, height 1s;
+    font-family: 'Quicksand', sans-serif;
+
+    &:hover {
+        width: 100vw;
+        height: 100vh;
+    }
+
 }
 </style>
